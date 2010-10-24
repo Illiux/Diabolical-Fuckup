bc37fd9f6e87a1dc70d6803993cbbba9bf6803f2
Nol <nol@razorandblade.org>
2010-10-24 03:12:58 -0400

    Reactivated collision

--- platform.cpp
+++ platform.cpp
@@ -44,10 +44,10 @@ Platform::Platform(float x, float y, int width, int height)
 	this->height = height;
 }
 
-//bool Platform::Collides(Player player) {
-//  if ((player.getY() + PLAYER_HEIGHT) < y) return false;
-//  if ((player.getX() + PLAYER_WIDTH) < x) return false;
-//  if ((y + height) < player.getY()) return false;
-//  if ((x + width) < player.getX()) return false;
-//  return true;
-//}
+bool Platform::Collides(Player player) {
+ if ((player.getY() + PLAYER_HEIGHT) < y) return false;
+ if ((player.getX() + PLAYER_WIDTH) < x) return false;
+ if ((y + height) < player.getY()) return false;
+ if ((x + width) < player.getX()) return false;
+ return true;
+}
