1ccd3837baf50ae3901c6cbf5d06143d05b81e05
Nol <nol@razorandblade.org>
2010-10-24 01:49:38 -0400

    Fixed

--- dfgame.h
+++ dfgame.h
@@ -1,7 +1,24 @@
+#ifndef _DFGAME_H_
+#define _DFGAME_H_
 
-void clean_up();
-bool init();
-bool init_GL();
+#include "dfgame.h"
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_opengl.h"
+#include "globals.h"
+
+class DFGame
+{
+private:
+	bool init_GL();
+	bool valid;
+	SDL_Event event;
+public:
+	DFGame();
+	~DFGame();
+	int MainLoop();
+	bool IsValid();
+};
 
 class Floor{
 private:
@@ -37,3 +54,5 @@ public:
 	bool is_paused();
 };
 */
+
+#endif
