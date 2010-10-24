1561bf792d5979ee0beededa119be32bd632bd46
Nol <nol@razorandblade.org>
2010-10-24 01:10:55 -0400

    Added globals.h

---
+++ globals.h
@@ -0,0 +1,11 @@
+#ifndef _DFGAME_GLOBALS_H_
+#define _DFGAME_GLOBALS_H_
+
+#define SCREEN_WIDTH	800
+#define SCREEN_HEIGHT 600
+#define SCREEN_BPP		32
+#define FLOOR_HEIGHT	50
+#define FLOOR_WIDTH		800
+#define FPS						60
+
+#endif
